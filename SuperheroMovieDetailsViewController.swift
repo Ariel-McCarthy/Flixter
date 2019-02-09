8b8e4d84ef69bb3976bbad3910fdd4644e06cd89
Ariel-McCarthy <somccarthy@csumb.edu>
2019-02-09 12:06:59 -0800

    updated README

---
+++ SuperheroMovieDetailsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SuperheroMovieDetailsViewController.swift
+//  
+//
+//  Created by Ariel McCarthy on 2/9/19.
+//
+
+import UIKit
+
+class SuperheroMovieDetailsViewController: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
